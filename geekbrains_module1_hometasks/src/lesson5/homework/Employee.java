a66580dea5e6ae6f106d2f06a007e7f9cfa964e3
Nikita Makarov <dev.nikita.makarov@gmail.com>
2022-01-27 01:54:42 +0300

    Lesson 5 homework added

---
+++ geekbrains_module1_hometasks/src/lesson5/homework/Employee.java
@@ -0,0 +1,30 @@
+package lesson5.homework;
+
+public class Employee {
+    public String fullName;
+    public String position;
+    public String email;
+    public String phoneNumber;
+    public double salary;
+    public int age;
+
+    public Employee(String fullName, String position, String email, String phoneNumber, double salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee: " + "\n" +
+                "Full name: " + fullName + '\n' +
+                "Position: " + position + '\n' +
+                "Email: " + email + '\n' +
+                "Phone number: " + phoneNumber + '\n' +
+                "Salary: " + salary + '\n' +
+                "Age: " + age;
+    }
+}
---
+++ geekbrains_module1_hometasks/src/lesson5/homework/Main.java
@@ -0,0 +1,20 @@
+package lesson5.homework;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Employee[] employees = new Employee[5];
+        employees[0] = new Employee("Alex", "Manager", "alex@sber.ru", "321", 320, 41);
+        employees[1] = new Employee("Nik", "Director", "nik@sber.ru", "123", 620, 32);
+        employees[2] = new Employee("Raf", "Developer", "raf@sber.ru", "456", 480, 45);
+        employees[3] = new Employee("Sergey", "Scientist", "sergey@sber.ru", "654", 150, 28);
+        employees[4] = new Employee("Ivan", "Account Manager", "ivan@sber.ru", "789", 200, 24);
+
+        for (Employee e: employees
+             ) {
+            if (e.age > 40) {
+                System.out.println(e + "\n");
+            }
+        }
+    }
+}
